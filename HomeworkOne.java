9e7aa08fb30a077f4a92226bed0163a43112ffe2
alexis-dimas <alexisdimas81@gmail.com>
2023-10-03 20:35:22 -0700

    Add draft Gale-Shapley Algorithm

--- HomeworkOne.java
+++ HomeworkOne.java
@@ -2,93 +2,81 @@ import java.util.*;
 public class HomeworkOne {
     public static void main(String[] args) {
         int[][] M = {
-                        {2, 1, 3, 0},
-                        {0, 1, 3, 2},
-                        {0, 1, 2, 3},
-                        {0, 1, 2, 3}
+                        {0, 1, 2},
+                        {1, 0, 2},
+                        {0, 1, 2}
                     };
         int[][] W = {
-                        {0, 2, 1, 3},
-                        {2, 0, 3, 1},
-                        {3, 2, 1, 0},
-                        {2, 3, 1, 0}
+                        {1, 2, 0},
+                        {0, 1, 2},
+                        {0, 1, 2}
                     };
 
-        questionFour(M, W);
+        galeShapleyAlgorithm(M, W);
     }
 
-    public static void questionFour(int[][] M, int[][] W) {
+    public static void galeShapleyAlgorithm(int[][] M, int[][] W) {
         Queue<Integer> unmatchedMen = new LinkedList<>();
-        Set<Integer> unmatchedWomen = new HashSet<>();
         Map<Integer, Integer> mRank = new HashMap<>();
-        initialize(M, W, unmatchedMen, unmatchedWomen, mRank);
-
+        initializeAll(M, unmatchedMen, mRank);
         Map<Integer, Integer> matches = new HashMap<>();
+        System.out.println("Trace:");
         while (!unmatchedMen.isEmpty()) {
             int m = unmatchedMen.poll();
             int[] mPreferenceList = M[m];
             int rank = mRank.get(m);
             int w = mPreferenceList[rank];
-
-            System.out.print(m + " proposes to " + w + " [" + w + "," + matches.getOrDefault(w, -1) + "]  ");
-            if (unmatchedWomen.contains(w)) {
-                System.out.print("Pair = " + "(" + m + "," + w + ") ");
-                System.out.print("Accepted");
+            System.out.print(m + " proposes to " + w + " [" + w + "," +
+                    matches.getOrDefault(w, -1) + "]  ");
+            if (!matches.containsKey(w)) {
+                System.out.println("Accepted");
                 matches.put(w, m);
-                unmatchedWomen.remove(w);
             } else {
                 int[] wPreferenceList = W[w];
-
-                // I think it has to do with the wPreferenceList[0]
-                if (getHighest(wPreferenceList, m, matches.get(w))) {
-                    System.out.print("Pair = " + "(" + m + "," + w + ") ");
-                    System.out.print("Accepted");
-                    int prevM = matches.get(w);
+                int prevMatch = matches.get(w);
+                if (wPrefersM(wPreferenceList, m, prevMatch)) {
+                    System.out.println("Accepted");
                     matches.put(w, m);
-                    unmatchedMen.add(prevM);
-                } else { // else w rejects m
-                    System.out.print("Rejected " + rank);
-                    unmatchedMen.add(m); // m remains free
+                    unmatchedMen.add(prevMatch);
+                } else {
+                    System.out.println("Rejected");
+                    unmatchedMen.add(m);
                 }
             }
 
-            System.out.println();
             rank++;
             mRank.put(m, rank);
         }
 
-        for (Map.Entry entry : matches.entrySet()) {
-            System.out.println("M = " + entry.getValue() + ", W = " + entry.getKey());
-        }
-
-
+        System.out.println();
+        printOutput(matches);
     }
 
-    public static void initialize(int[][] M, int[][] W, Queue<Integer> unmatchedMen,
-                                  Set<Integer> unmatchedWomen, Map<Integer, Integer> mRank) {
-        for (int r = 0; r < M.length; r++) {
-            unmatchedMen.add(r);
-            mRank.put(r, 0);
-        }
-
-        for (int r = 0; r < W.length; r++) {
-            unmatchedWomen.add(r);
+    public static void initializeAll(int[][] M, Queue<Integer> unmatchedMen,
+                                     Map<Integer, Integer> mRank) {
+        for (int i = 0; i < M.length; i++) {
+            unmatchedMen.add(i);
+            mRank.put(i, 0);
         }
     }
 
-    public static boolean getHighest(int[] preferenceList, int curr, int prev) {
-
-        // which does it encounter first?
-        boolean value = false;
-        for (int i = 0; i < preferenceList.length; i++) {
-            if (preferenceList[i] == curr) {
-                value = true;
-                break;
-            } else if (preferenceList[i] == prev) {
-                break;
-            }
+    public static boolean wPrefersM(int[] wPreferenceList, int currM, int prevMatch) {
+        for (int m : wPreferenceList) {
+            if (m == currM) return true;
+            if (m == prevMatch) break;
         }
 
-        return value;
+        return false;
+    }
+
+    public static void printOutput(Map<Integer, Integer> matches) {
+        int[] output = new int[matches.size()];
+        for (int w = 0; w < output.length; w++) {
+            int m = matches.get(w);
+            output[m] = w;
+        }
+
+        System.out.println("Output:");
+        System.out.println(Arrays.toString(output));
     }
 }
