49f76ec948462c6e9beadf89743ab150b49f8d85
alexis-dimas <alexisdimas81@gmail.com>
2023-10-05 15:20:42 -0700

    Add random preference list algorithm

--- HomeworkOne.java
+++ HomeworkOne.java
@@ -2,17 +2,26 @@ import java.util.*;
 public class HomeworkOne {
     public static void main(String[] args) {
         int[][] M = {
-                        {0, 1, 2},
-                        {1, 0, 2},
-                        {0, 1, 2}
+                        {2, 1, 3, 0},
+                        {0, 1, 3, 2},
+                        {0, 1, 2, 3},
+                        {0, 1, 2, 3}
                     };
         int[][] W = {
-                        {1, 2, 0},
-                        {0, 1, 2},
-                        {0, 1, 2}
+                        {0, 2, 1, 3},
+                        {2, 0, 3, 1},
+                        {3, 2, 1, 0},
+                        {2, 3, 1, 0}
                     };
 
-        galeShapleyAlgorithm(M, W);
+        // galeShapleyAlgorithm(M, W);
+
+        int n = 10;
+        int[][] randomM = randomPreferenceList(n);
+        printMatrix(randomM);
+        System.out.println();
+        int[][] randomW = randomPreferenceList(n);
+        printMatrix(randomW);
     }
 
     public static void galeShapleyAlgorithm(int[][] M, int[][] W) {
@@ -78,5 +87,40 @@ public class HomeworkOne {
 
         System.out.println("Output:");
         System.out.println(Arrays.toString(output));
+    }
+
+    public static int[][] randomPreferenceList(int n) {
+        int[][] randomMatrix = new int[n][n];
+        int[] nums = new int[n];
+        for (int i = 0; i < nums.length; i++) {
+            nums[i] = i;
+        }
+
+        for (int r = 0; r < randomMatrix.length; r++) {
+            int[] shuffled = shuffle(nums);
+            for (int c = 0; c < randomMatrix[0].length; c++) {
+                randomMatrix[r][c] = shuffled[c];
+            }
+        }
+
+        return randomMatrix;
+    }
+
+    public static int[] shuffle(int[] arr) {
+        Random rand = new Random();
+        for (int i = arr.length - 1; i > 0; i--) {
+            int index = rand.nextInt(i + 1);
+            int temp = arr[index];
+            arr[index] = arr[i];
+            arr[i] = temp;
+        }
+
+        return arr;
+    }
+
+    public static void printMatrix(int[][] matrix) {
+        for (int[] row : matrix) {
+            System.out.println(Arrays.toString(row));
+        }
     }
 }
