8dd1233804e31e8f1b727dccde3f8e61507ac4ff
alexis-dimas <alexisdimas81@gmail.com>
2023-10-05 20:41:07 -0700

    Add final GS algorithm and goodness calculator

--- HomeworkOne.java
+++ HomeworkOne.java
@@ -14,67 +14,67 @@ public class HomeworkOne {
                         {2, 3, 1, 0}
                     };
 
-        // galeShapleyAlgorithm(M, W);
-
-        int n = 10;
+        int n = 1000;
         int[][] randomM = randomPreferenceList(n);
-        printMatrix(randomM);
-        System.out.println();
         int[][] randomW = randomPreferenceList(n);
-        printMatrix(randomW);
+        int[] mRankOfM = new int[randomM.length];
+        int[] wRankOfW = new int[randomW.length];
+        galeShapleyAlgorithm(randomM, randomW, mRankOfM, wRankOfW);
+        defineGoodness(mRankOfM, wRankOfW, n);
     }
 
-    public static void galeShapleyAlgorithm(int[][] M, int[][] W) {
+    public static void galeShapleyAlgorithm(int[][] M, int[][] W, int[] mRankOfM, int[] wRankOfW) {
         Queue<Integer> unmatchedMen = new LinkedList<>();
-        Map<Integer, Integer> mRank = new HashMap<>();
-        initializeAll(M, unmatchedMen, mRank);
+        Map<Integer, Integer> mPositionInList = new HashMap<>();
+        initializeAll(M, unmatchedMen, mPositionInList);
         Map<Integer, Integer> matches = new HashMap<>();
         System.out.println("Trace:");
         while (!unmatchedMen.isEmpty()) {
             int m = unmatchedMen.poll();
             int[] mPreferenceList = M[m];
-            int rank = mRank.get(m);
-            int w = mPreferenceList[rank];
-            System.out.print(m + " proposes to " + w + " [" + w + "," +
-                    matches.getOrDefault(w, -1) + "]  ");
+            int mPosition = mPositionInList.get(m);
+            int w = mPreferenceList[mPosition];
+            int[] wPreferenceList = W[w];
+            System.out.print(m + " proposes to " + w + " [" + w + "," + matches.getOrDefault(w, -1) + "]  ");
             if (!matches.containsKey(w)) {
                 System.out.println("Accepted");
                 matches.put(w, m);
+                int wPosition = findM(wPreferenceList, m);
+                mRankOfM[m] = mPosition + 1;
+                wRankOfW[w] = wPosition + 1;
             } else {
-                int[] wPreferenceList = W[w];
                 int prevMatch = matches.get(w);
-                if (wPrefersM(wPreferenceList, m, prevMatch)) {
+                if (wPrefersThisM(wPreferenceList, m, prevMatch)) {
                     System.out.println("Accepted");
                     matches.put(w, m);
                     unmatchedMen.add(prevMatch);
+                    int wPosition = findM(wPreferenceList, m);
+                    mRankOfM[m] = mPosition + 1;
+                    wRankOfW[w] = wPosition + 1;
                 } else {
                     System.out.println("Rejected");
                     unmatchedMen.add(m);
                 }
             }
-
-            rank++;
-            mRank.put(m, rank);
+            mPosition++;
+            mPositionInList.put(m, mPosition);
         }
-
         System.out.println();
         printOutput(matches);
     }
 
-    public static void initializeAll(int[][] M, Queue<Integer> unmatchedMen,
-                                     Map<Integer, Integer> mRank) {
+    public static void initializeAll(int[][] M, Queue<Integer> unmatchedMen, Map<Integer, Integer> mPositionInList) {
         for (int i = 0; i < M.length; i++) {
             unmatchedMen.add(i);
-            mRank.put(i, 0);
+            mPositionInList.put(i, 0);
         }
     }
 
-    public static boolean wPrefersM(int[] wPreferenceList, int currM, int prevMatch) {
+    public static boolean wPrefersThisM(int[] wPreferenceList, int currM, int prevMatch) {
         for (int m : wPreferenceList) {
             if (m == currM) return true;
             if (m == prevMatch) break;
         }
-
         return false;
     }
 
@@ -84,7 +84,6 @@ public class HomeworkOne {
             int m = matches.get(w);
             output[m] = w;
         }
-
         System.out.println("Output:");
         System.out.println(Arrays.toString(output));
     }
@@ -102,7 +101,6 @@ public class HomeworkOne {
                 randomMatrix[r][c] = shuffled[c];
             }
         }
-
         return randomMatrix;
     }
 
@@ -114,13 +112,32 @@ public class HomeworkOne {
             arr[index] = arr[i];
             arr[i] = temp;
         }
-
         return arr;
     }
 
-    public static void printMatrix(int[][] matrix) {
-        for (int[] row : matrix) {
-            System.out.println(Arrays.toString(row));
+    public static int findM(int[] wPreferenceList, int m) {
+        int index = 0, l = 0, r = wPreferenceList.length - 1;
+        while (r >= l) {
+            if (wPreferenceList[l] == m) {
+                index = l;
+                break;
+            } else if (wPreferenceList[r] == m) {
+                index = r;
+                break;
+            }
+
+            l++;
+            r--;
         }
+        return index;
+    }
+
+    public static void defineGoodness(int[] mRankOfM, int[] wRankOfW, int n) {
+        int mSum = 0;
+        int wSum = 0;
+        for (int mVal : mRankOfM) mSum += mVal;
+        for (int wVal : wRankOfW) wSum += wVal;
+        System.out.println();
+        System.out.println("MGoodness = " + mSum / n + ", WGoodness = " + wSum / n);
     }
 }
